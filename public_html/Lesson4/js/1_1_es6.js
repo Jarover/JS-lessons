1dcea05a47db36cdd7f63450321bd5aee976d7a7
Jaroslav <yaroslav.ananchenko@gmail.com>
2021-02-27 13:47:15 +0300

    Lesson4 done

---
+++ public_html/Lesson4/js/1_1_es6.js
@@ -0,0 +1,14 @@
+"use strict";
+
+class Product {
+
+    constructor(name, price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    make25PercentDiscount() {
+
+        this.price = this.price * 0.75;
+    }
+}
